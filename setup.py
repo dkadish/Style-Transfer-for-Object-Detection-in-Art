4de278574541f149b07b9fee1355a8de2e5cbe57
David Kadish <david.kadish@gmail.com>
2021-03-10 15:46:50 +0100

    Version rename

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ArtDetect',
-    version='0.1',
+    version='0.1.0',
     packages=['artdetect', 'artdetect.ignite', 'artdetect.utils'],
     url='https://github.com/dkadish/ArtNet2',
     license='GPL-3.0',
